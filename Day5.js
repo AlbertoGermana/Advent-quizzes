3e1ecd7787947db33a8faaf4ed9b1d5c9d6dbd6a
Alberto <alberto.germana@gmail.com>
2022-12-12 08:37:27 +0100

    Day5 part2

--- Day5.js
+++ Day5.js
@@ -1,4 +1,3 @@
-
 moves = moves.split('\n').map((c,i) => {
     let a = c.replace("move ", "").split('from');
     let b = a[1].split("to")
@@ -15,10 +14,12 @@ for (let i = 0; i < moves.length; i++) {
     let column = boxes_pos[m.from -1];
     let to_move = m.to_move;
     let remain_length = column.length - to_move;
+    
+    //the next line is the only difference between part1 and part2 solutions
+    let result = column.slice(-to_move).split("").reverse().join("");
+    // let result = column.slice(-to_move)
 
-    let result = column.slice(-to_move).split("").reverse().join("")
-    column = column.substring(0, remain_length)
-
+    column = column.substring(0, remain_length);
     boxes_pos[m.from -1] = column; // what remain of prev column
     boxes_pos[m.to -1] = boxes_pos[m.to -1] + result; // what is in new column
 }
@@ -28,4 +29,7 @@ for (let i = 0; i < boxes_pos.length; i++) {
     const col = boxes_pos[i];
     result += col.substring( col.length -1)
 }
-console.log("Day5 - part1: -->", result)
+// to see the real result just uncomment line 35 and switch lines 19-20 from comment
+console.log("Day5 - part1: -->", "VQZNJMWTR")
+console.log("Day5 - part2: -->", "NLCDCLVMQ")
+// console.log("Day5 solution: ", result)
